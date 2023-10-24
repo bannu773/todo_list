b740a99b480f99ed4ff52464cf4e5a335e6a6b4a
bannu773 <bandibannu773@gmail.com>
2023-10-24 14:37:26 +0530

    first

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -7,160 +7,160 @@ import Todo from '../components/Todo';
 import { AuthContext } from '../context/AuthContext';
 
 const Todolist = () => {
+    const baseUrl = 'https://todo-list-15i9.onrender.com/';
 
     const [value, setValue] = useState({
         title: '',
         text: '',
-    })
+    });
 
-    const [todos, setTodos] = useState([])
-    const [mod, setMod] = useState({is:'create',id:''})
+    const [todos, setTodos] = useState([]);
+    const [mod, setMod] = useState({ is: 'create', id: '' });
 
-
-    const {id} = useContext(AuthContext)
+    const { id } = useContext(AuthContext);
 
     const getTodos = async (token) => {
         try {
-            
-            await axios.get('/todo',{
-                headers:{
+            await axios.get(`${baseUrl}/todo`, {
+                headers: {
                     'Content-Type': 'application/json'
                 },
                 cancelToken: token,
-                params: {id}
+                params: { id }
             })
-            .then(response => setTodos(response.data))
+                .then(response => setTodos(response.data));
 
         } catch (error) {
-            if(axios.isCancel(error)){
-                return
-            }else{
+            if (axios.isCancel(error)) {
+                return;
+            } else {
                 console.log(error);
             }
         }
-    }
-    useEffect(()=>{
-        let source = axios.CancelToken.source()
+    };
 
-        getTodos(source.token)
+    useEffect(() => {
+        let source = axios.CancelToken.source();
 
-        return () => {source.cancel()}
-    })
+        getTodos(source.token);
+
+        return () => { source.cancel(); };
+    }, []);
 
     const createTodo = async () => {
         try {
-            
-            if(value.title.length === 0){
-                return
+
+            if (value.title.length === 0) {
+                return;
             }
-            if(value.text.length === 0){
-                return
+            if (value.text.length === 0) {
+                return;
             }
 
-            await axios.post('/todo/add', {...value, id}, {
-                headers:{
+            await axios.post(`${baseUrl}/todo/add`, { ...value, id }, {
+                headers: {
                     'Content-Type': 'application/json'
                 }
             })
-            .then(response => {
-                setTodos([...todos, response.data])
-            })
-            .then(setValue({title: '' , text: ''}))
+                .then(response => {
+                    setTodos([...todos, response.data]);
+                })
+                .then(setValue({ title: '', text: '' }));
 
         } catch (error) {
             console.log(error);
         }
-    }
+    };
 
     const deleteTodo = async (id) => {
         try {
-            
-            await axios.delete(`/todo/delete/${id}`, {id},{
-                headers:{
+
+            await axios.delete(`${baseUrl}/todo/delete/${id}`, { id }, {
+                headers: {
                     'Content-Type': 'application/json'
                 }
-                }).then(getTodos())
+            }).then(getTodos());
 
         } catch (error) {
             console.log(error);
         }
-    } 
+    };
 
-    function updateAll(){
-        setValue({title: '', text: ''})
-        setMod({is:'create',id:''})
+    function updateAll() {
+        setValue({ title: '', text: '' });
+        setMod({ is: 'create', id: '' });
     }
 
     const updateTodo = async (id) => {
         try {
-            
-            await axios.post(`/todo/update/${id}`, {...value}, {
-                headers:{
+            await axios.post(`${baseUrl}/todo/update/${id}`, { ...value }, {
+                headers: {
                     'Content-Type': 'application/json'
                 }
             })
-            .then(updateAll())
+                .then(updateAll());
 
         } catch (error) {
             console.log(error);
         }
-    }
+    };
+
     const changeTodo = async (id) => {
         try {
-            
-            await axios.get(`/todo/change/${id}`, {id}, {
-                headers:{
+
+            await axios.get(`${baseUrl}/todo/change/${id}`, { id }, {
+                headers: {
                     'Content-Type': 'application/json'
                 }
-            }).then(response=>{
-                setValue({title: response.data.title , text: response.data.text})
-                setMod({is:'update',id:id})
-            })
+            }).then(response => {
+                setValue({ title: response.data.title, text: response.data.text });
+                setMod({ is: 'update', id: id });
+            });
 
         } catch (error) {
             console.log(error);
         }
-    }
+    };
 
 
     return (
         <main className='TodoList'>
-            <Navbar/>
-                <section className='todoContent'>
-                    <form onSubmit={e => e.preventDefault()}>
-                        <div>
-                            <label>Title: </label>
-                            <input autoComplete="off" value={value.title} onChange={e => setValue({...value,title: e.target.value})} maxLength={16} type="text" placeholder='Your title here...'/>
-                        </div>
-                        <div>
-                            <label>Text: </label>
-                            <input autoComplete="off" value={value.text} onChange={e => setValue({...value,text: e.target.value})} maxLength={150} type="text" placeholder='Your text here...'/>
-                        </div>
-                        <div>
-                            {mod.is === 'update' ? <><button onClick={()=>updateTodo(mod.id)} >Change</button><button onClick={updateAll} >Cancel</button></> : <button onClick={createTodo} >Add Todo</button>}
-                        </div>
-                    </form>
-                    <div className="todos">
-                        {
-                            todos.map((todoValue, index) => {
-                                return(
-                                    <Todo
-                                     title={todoValue.title} 
-                                     text={todoValue.text} 
-                                     index={index + 1} 
-                                     key={index} 
-                                     deleteFunc={() => deleteTodo(todoValue._id)}
-                                     changeFunc={()=> changeTodo(todoValue._id)} 
-                                     />
-                                )
-                            })
-                        }
+            <Navbar />
+            <section className='todoContent'>
+                <form onSubmit={e => e.preventDefault()}>
+                    <div>
+                        <label>Title: </label>
+                        <input autoComplete="off" value={value.title} onChange={e => setValue({ ...value, title: e.target.value })} maxLength={16} type="text" placeholder='Your title here...' />
                     </div>
-                </section>
-            <Footer/>
-            <Homebutton/>
+                    <div>
+                        <label>Text: </label>
+                        <input autoComplete="off" value={value.text} onChange={e => setValue({ ...value, text: e.target.value })} maxLength={150} type="text" placeholder='Your text here...' />
+                    </div>
+                    <div>
+                        {mod.is === 'update' ? <><button onClick={() => updateTodo(mod.id)} >Change</button><button onClick={updateAll} >Cancel</button></> : <button onClick={createTodo} >Add Todo</button>}
+                    </div>
+                </form>
+                <div className="todos">
+                    {
+                        todos.map((todoValue, index) => {
+                            return (
+                                <Todo
+                                    title={todoValue.title}
+                                    text={todoValue.text}
+                                    index={index + 1}
+                                    key={index}
+                                    deleteFunc={() => deleteTodo(todoValue._id)}
+                                    changeFunc={() => changeTodo(todoValue._id)}
+                                />
+                            );
+                        })
+                    }
+                </div>
+            </section>
+            <Footer />
+            <Homebutton />
         </main>
     );
-}
+};
 
 export default Todolist;
