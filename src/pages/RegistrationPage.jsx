e4b6ce8b8863080ecb751c174e7a1a063e61e6e3
bannu773 <bandibannu773@gmail.com>
2023-10-24 14:44:11 +0530

    second

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,76 +1,72 @@
 import React, { useContext, useState } from 'react';
-import {Link , useNavigate} from 'react-router-dom'
-import axios from 'axios'
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import { AuthContext } from '../context/AuthContext';
 import Homebutton from '../components/HomeButton';
 
 const Registrationpage = () => {
+    const baseUrl = 'https://todo-list-15i9.onrender.com/';
 
-    const navigate = useNavigate()
-    const {login} = useContext(AuthContext) 
+    const navigate = useNavigate();
+    const { login } = useContext(AuthContext);
 
     const [form, setForm] = useState({
-        name:'',
+        name: '',
         password: ''
-    })
+    });
 
-    const changeHandler = (event)=>{
-        setForm({...form, [event.target.name]: event.target.value})
-    }
-    
-    const registerHandler = async () =>{
+    const changeHandler = (event) => {
+        setForm({ ...form, [event.target.name]: event.target.value });
+    };
+
+    const registerHandler = async () => {
         try {
-            await axios.post('/auth/registration', {...form}, {
-                headers:{
+            await axios.post(`${baseUrl}/auth/registration`, { ...form }, {
+                headers: {
                     'Content-Type': 'application/json'
                 }
             })
-            
-            .then(
-                async () => {
-                        await axios.post('/auth/login', {...form}, {
-                            headers:{
-                                'Content-Type': 'application/json'
-                            }
-                        }).then(response => {
-                            login(response.data.id)
-                        })
-                    }
-                )
-
-            .then(()=>{
-                navigate('/')
-            })
-        } 
-        catch (error) {
+                .then(async () => {
+                    await axios.post(`${baseUrl}/auth/login`, { ...form }, {
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    }).then(response => {
+                        login(response.data.id);
+                    });
+                })
+                .then(() => {
+                    navigate('/');
+                });
+        } catch (error) {
             console.log(error);
-        }   
-    }
+        }
+    };
 
     return (
         <React.Fragment>
             <main className="login">
-            <section className="auth">
-                <header>Todo List - Create Account</header>
-                <form className='loginForm'>
-                    <div>
-                        <label>Name:</label>
-                        <input autoComplete="off" onChange={changeHandler} name='name' placeholder='Max length 16...' type="text" className="validate" maxLength={'16'}/>
-                    </div>
-                    <div>
-                        <label>Password:</label>
-                        <input autoComplete="off" onChange={changeHandler} name='password' placeholder='Max length 16...' type="text" className="validate" maxLength={'16'}/>
-                    </div>
-                </form>
-                <footer> 
-                    <Link to={'/login'}>I have Acaunt</Link>
-                    <button onClick={registerHandler}>Create</button>
-                </footer>
-            </section>
+                <section className="auth">
+                    <header>Todo List - Create Account</header>
+                    <form className='loginForm'>
+                        <div>
+                            <label>Name:</label>
+                            <input autoComplete="off" onChange={changeHandler} name='name' placeholder='Max length 16...' type="text" className="validate" maxLength={'16'} />
+                        </div>
+                        <div>
+                            <label>Password:</label>
+                            <input autoComplete="off" onChange={changeHandler} name='password' placeholder='Max length 16...' type="text" className="validate" maxLength={'16'} />
+                        </div>
+                    </form>
+                    <footer>
+                        <Link to={'/login'}>I have Acaunt</Link>
+                        <button onClick={registerHandler}>Create</button>
+                    </footer>
+                </section>
             </main>
-            <Homebutton/>
+            <Homebutton />
         </React.Fragment>
     );
-}
+};
 
 export default Registrationpage;
